7dda83d30fa4fa3237aa92cb4a624ba4a896cb63
niharn22 <96176811+niharn22@users.noreply.github.com>
2022-12-11 17:51:04 +0530

    Update producer consumer.c

--- producer consumer.c
+++ producer consumer.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 int mutex=1,full=0,empty=10,x=0;
 void producer()
 {
@@ -23,6 +22,8 @@ void consumer()
 void main()
 {
     int i,ch;
+    do
+    {
     printf("\n1.Producer\n2.Consumer\n3.Exit\nEnter your choice:");
     scanf("%d",&ch);
     switch(ch)
@@ -30,14 +31,16 @@ void main()
         case 1:if((mutex==1)&&(empty!=0))
         producer();
         else
-        printf("\nBuffer is Full");
+        printf("Buffer is Full\n");
         break;
         case 2:if((mutex==1)&&(empty!=0))
         consumer();
         else
-        printf("\nBuffer is Empty");
+        printf("Buffer is Empty\n");
         break;
-        case 3:printf("Thank You!!");
+        case 3:printf("Thank You!!\n");
         break;
+        default:printf("Invalid Input!!\n");
     }
+    }while(ch!=3);
 }
